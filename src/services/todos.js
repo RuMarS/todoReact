983bb63f73e815a24a517012dbab9440c1f33aad
RuMarS <user.24.12.2010@gmail.com>
2022-11-18 08:55:14 +0400

    todo commit

---
+++ src/services/todos.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+class TodoDataService {
+    getAll(token) {
+        axios.defaults.headers.common["Authorization"] = `Token ${token}` ;//"Token"+ token;
+        //alert( "axios "+ axios.defaults.headers.common["Authorization"]);
+        return axios.get("http://user24122010.pythonanywhere.com/api/todos/"); // localhost:8000 -> user24122010.pythonanywhere.com
+    }
+
+    createTodo(data, token) {
+        axios.defaults.headers.common["Authorization"] = `Token ${token}`;
+       // alert("axios createTodo");
+        return axios.post("http://user24122010.pythonanywhere.com/api/todos/", data);
+    }
+
+    updateTodo(id, data, token) {
+        axios.defaults.headers.common["Authorization"] = `Token ${token}`;
+      //  alert("axios updateTodo");
+        return axios.put("http://user24122010.pythonanywhere.com/api/todos/"+`${id}`, data);
+    }
+
+    deleteTodo (id, token) {
+        axios.defaults.headers.common["Authorization"] = `Token ${token}`;
+        return axios.delete("http://user24122010.pythonanywhere.com/api/todos/"+`${id}`);
+    }
+
+    completeTodo(id, token){
+        axios.defaults.headers.common["Authorization"] = `Token ${token}`;
+        return axios.put("http://user24122010.pythonanywhere.com/api/todos/"+`${id}`+"/complete");
+    }
+
+    login(data){
+        return axios.post("http://user24122010.pythonanywhere.com/api/login/", data);
+    }
+
+    signup(data){
+        return axios.post("http://user24122010.pythonanywhere.com/api/signup/", data);
+    }
+
+}
+
+export default new TodoDataService();
